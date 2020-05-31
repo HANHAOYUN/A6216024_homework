c40f9738f30b5138384e10e0a5c3a2d5c54ef1ad
colalai <64138413+colalai@users.noreply.github.com>
2020-05-31 10:55:41 +0800

    Add files via upload

--- poker.h
+++ poker.h
@@ -1,57 +1,28 @@
-
-
 #ifndef POKER_H_
-
 #define POKER_H_
-#include<iostream>
-#include<stdlib.h>
 #include<string>
-#include<time.h>
-#include<sstream>
-#include<limits>
 using namespace std;
 
-
-
 class Poker
-
 {
-
 public:
-
 	Poker();
-
 	void shuffle();
-
 	string getPlayer();
-
 	string getLandlord();
-
 	int getSumP();
-
 	int getSumL();
-
 	void playerAsk();
-
 	void landlordAsk();
-
 	void newGame();
-
 	void landlordProcess();
 
-
-
 private:
-
 	int poker[53], pValue[53];
-
 	string pName[53];
-
 	int player[5], landlord[5];
-
 	int playerN, landlordN;
-
-
 
 };
 #endif
+
